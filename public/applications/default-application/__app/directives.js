a96ee3c723af02756875fc351ffdc7f92c59fcf4
Михаил Воронцов <vmm90@yandex.ru>
2017-01-28 22:29:33 +0700

    directive

--- public/applications/default-application/__app/directives.js
+++ public/applications/default-application/__app/directives.js
@@ -2,25 +2,25 @@
 
 (function (angular, appName) {
 
-    // angular.module(appName).directive('headTitle', headTitle);
-    //
-    // headTitle.$inject = ['$rootScope', '$timeout'];
-    // function headTitle($rootScope, $timeout) {
-    //     return {
-    //         link: function (scope, element) {
-    //             var listener = function (event, toState, toParams, fromState, fromParams) {
-    //                 var title = '';
-    //
-    //                 if (toState.data && toState.data.headTitle)
-    //                     title = toState.data.headTitle;
-    //                 $timeout(function () {
-    //                     element.text(title);
-    //                 });
-    //             };
-    //
-    //             $rootScope.$on('$stateChangeStart', listener);
-    //         }
-    //     };
-    // }
+    angular.module(appName).directive('headTitle', headTitle);
+
+    headTitle.$inject = ['$rootScope', '$timeout'];
+    function headTitle($rootScope, $timeout) {
+        return {
+            link: function (scope, element) {
+                var listener = function (event, toState, toParams, fromState, fromParams) {
+                    var title = '';
+
+                    if (toState.data && toState.data.headTitle)
+                        title = toState.data.headTitle;
+                    $timeout(function () {
+                        element.text(title);
+                    });
+                };
+
+                $rootScope.$on('$stateChangeStart', listener);
+            }
+        };
+    }
 
 }(angular, appName));
